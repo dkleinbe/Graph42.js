c8574f2d244ed778af95bbfef495e5a40386529d
denis <dkleinbe@free.fr>
2017-12-24 19:29:22 +0100

    added relation type on links

--- src/view/GraphEditor.js
+++ src/view/GraphEditor.js
@@ -11,7 +11,7 @@ Class to access GraphEditor
 **/
 export class GraphEditor {
   /**
-   **/
+  **/
   constructor(svg, graph) {
 
     this._tickListeners = [];
@@ -19,24 +19,24 @@ export class GraphEditor {
     this._graph = graph;
 
     var width = parseInt(svg.style('width')),
-      height = parseInt(svg.style('height'));
+    height = parseInt(svg.style('height'));
 
     this._dragline = svg.append('svg:path')
-      .attr('class', 'link dragline hidden')
-      .attr('d', 'M0,0L0,0');
+    .attr('class', 'link dragline hidden')
+    .attr('d', 'M0,0L0,0');
     //.style('marker-end', 'url(#mark-end-arrow)');
 
     // listen for key events
     d3.select(window)
-      .on("keydown", () => { this.onKeyDown(); })
-      .on("keyup", () => { this.onKeyUp(); });
+    .on("keydown", () => { this.onKeyDown(); })
+    .on("keyup", () => { this.onKeyUp(); });
 
     // list for mouse button  
     this._svg.on("mouseup", () => { this.onMouseUp(); });
 
 
     this._forceSim = d3.forceSimulation().force("charge", d3.forceManyBody(-200))
-      .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("center", d3.forceCenter(width / 2, height / 2));
     this._forceSim.force("charge").strength(-100);
     
     this._forceSim.force("link", d3.forceLink());
@@ -48,20 +48,20 @@ export class GraphEditor {
     this._relationSelection = null;
     this._targetNode = null;
     this.initFSM();
-  }
+}
   /**
-   **/
+  **/
   initFSM() {
     //
     this._fsm = new GraphEditorSM(this);
     this._fsm.init();
-  }
+}
   /**
-   **/
+  **/
   renderGraph(graph) {
     this._graph = graph;
     this.render();
-  }
+}
   /**
   **/
   render() {
@@ -76,21 +76,31 @@ export class GraphEditor {
 
     links.exit().remove();
 
-    var newLinks = links.enter()
-      .append("line")
-      .attr("class", "link")
-      .on("mouseover", (d, i, links) => { this._fsm.evaluate("mouseover_link", d, links[i]); })
-      .on("mouseleave", (d, i, links) => { this._fsm.evaluate("mouseleave_link", d, links[i]); })
-      .on("click", (d, i, links) => { if (d3.event.defaultPrevented) return;
-        this._fsm.evaluate("click", d, links[i]); })
-      .merge(links);
+    var newLinks = links.enter().append("g")
+        .attr("class", "link")
+        .on("mouseover", (d, i, links) => { this._fsm.evaluate("mouseover_link", d, links[i]); })
+        .on("mouseleave", (d, i, links) => { this._fsm.evaluate("mouseleave_link", d, links[i]); })
+        .on("click", (d, i, links) => { if (d3.event.defaultPrevented) return;
+            this._fsm.evaluate("click", d, links[i]); });
+
+    var path = newLinks.append("path")
+        .attr("id", (d,i) => { return 'edgepath'+i });
+
+    var edgeLabels = newLinks.append('text')
+        .attr("dx", "80");
+
+    edgeLabels.append('textPath')
+        .attr('xlink:href', (d,i) => { return '#edgepath'+i })
+        .text(d => { return d.type; });
 
     newLinks.append("title")
-      .text(d => {
-        return d.identity;
-      });
-      
+        .text(d => {
+            return d.identity;
+        });
+
     links = links.merge(newLinks);
+    path = links.selectAll("path");
+    edgeLabels = links.selectAll("text");
     this._links = links;
     //
     // Update nodes
@@ -104,7 +114,7 @@ export class GraphEditor {
     var newNodes = nodes.enter().append("g")
         .attr("class", d => {
             return "node " + d.label + " new"
-          })
+        })
         .call(d3.drag()
           .on("start", (d, i, nodes) => { d3.event.sourceEvent.stopPropagation();
             this._fsm.evaluate("drag_node_started", d, nodes[i]) })
@@ -115,7 +125,7 @@ export class GraphEditor {
         .on("mouseover", (d, i, nodes) => { this._fsm.evaluate("mouseover_node", d, nodes[i]); })
         .on("mouseleave", (d, i, nodes) => { this._fsm.evaluate("mouseleave_node", d, nodes[i]); })
         .on("click", (d, i, nodes) => { if (d3.event.defaultPrevented) return;
-           this._fsm.evaluate("click", d, nodes[i]); })
+         this._fsm.evaluate("click", d, nodes[i]); })
         .on("dblclick", (d, i, nodes) => { this._fsm.evaluate("dblclick", d, nodes[i]); });
     //
     // node circle
@@ -125,27 +135,27 @@ export class GraphEditor {
     // node text
     //
     newNodes.each(function(n,j) {
-      
-      let g = d3.select(this);
-      var words = ["no name"];
-      if (n.properties.hasOwnProperty('name')) {
-        words = n.properties.name.split(/\s+/g)    
-      }
-      let t = g.append("text").attr("text-anchor","middle");
 
-      words.forEach((w,i) => {
-        let tspan = t.append('tspan').text(w);
-        if (i > 0)
-          tspan.attr('x', 0).attr('dy', '15');
-      });
+        let g = d3.select(this);
+        var words = ["no name"];
+        if (n.properties.hasOwnProperty('name')) {
+            words = n.properties.name.split(/\s+/g)    
+        }
+        let t = g.append("text").attr("text-anchor","middle");
+
+        words.forEach((w,i) => {
+            let tspan = t.append('tspan').text(w);
+            if (i > 0)
+                tspan.attr('x', 0).attr('dy', '15');
+        });
     });
     //
     // node tooltip
     //
     newNodes.append("title")
-      .text(d => {
-        return d.identity;
-      });
+        .text(d => {
+            return d.identity;
+    });
     //
     // merge nodes
     //
@@ -155,27 +165,25 @@ export class GraphEditor {
     // force feed algo ticks
     //
     this._forceSim.on("tick", () => {
-      links.attr("x1", d => {
-        return d.source.x;
-      }).attr("y1", d => {
-        return d.source.y;
-      }).attr("x2", d => {
-        return d.target.x;
-      }).attr("y2", d => {
-        return d.target.y;
-      });
+        path.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
+                                           //console.log(d)
+                                           return path});
+        edgeLabels.attr('transform',(d,i, labels) => {
+            if (d.target.x<d.source.x) {
+                let bbox = labels[i].getBBox();
+                let rx = bbox.x+bbox.width/2;
+                let ry = bbox.y+bbox.height/2;
+                return 'rotate(180 '+rx+' '+ry+')';
+            } else {
+                return 'rotate(0)';
+            }
+        });
 
-      nodes.attr("transform", function(d, i) {     
-        return "translate(" + d.x + "," + d.y + ")"; 
-      });
-      /*
-      nodes.attr("cx", d => {
-        return d.x;
-      }).attr("cy", d => {
-        return d.y;
-      });
-      */
-      this._tickListeners.map((listener) => listener(this._forceSim.alpha()));
+        nodes.attr("transform", function(d, i) {     
+            return "translate(" + d.x + "," + d.y + ")"; 
+        });
+
+        this._tickListeners.map((listener) => listener(this._forceSim.alpha()));
     });
     //
     // restart simulation
@@ -184,79 +192,79 @@ export class GraphEditor {
     this._forceSim.force("link").links(graph.links);
     if (graph.links.length == 0)
       this._forceSim.force("charge").strength(-10);
-    else
+  else
       this._forceSim.force("charge").strength(-100);
-    this._forceSim = this._forceSim.alpha(0.3).restart();
-  }
+  this._forceSim = this._forceSim.alpha(0.3).restart();
+}
 
-  isShiftDown() {
+isShiftDown() {
     return (d3.event.sourceEvent !== undefined ? d3.event.sourceEvent.shiftKey : d3.event.shiftKey);
-  }
+}
 
-  isCtrlDown() {
+isCtrlDown() {
     return d3.event.ctrlKey;
-  }
+}
 
-  isSelectedNode(n) {
+isSelectedNode(n) {
     return n === this._nodeSelection;
-  }
+}
 
-  filterDrag() {
+filterDrag() {
     return true;
-  }
+}
 
-  unPinNode(d, n) {
+unPinNode(d, n) {
 
     if (!d3.event.active) this._forceSim.alphaTarget(0.0001).restart();
     d.fx = null;
     d.fy = null;
-  }
+}
 
-  dragstarted(d, n) {
+dragstarted(d, n) {
 
     if (!d3.event.active) this._forceSim.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
-  }
+}
 
-  dragged(d) {
+dragged(d) {
     d.fx = d3.event.x;
     d.fy = d3.event.y;
-  }
+}
 
-  dragended(d, n) {
+dragended(d, n) {
 
     if (!d3.event.active) this._forceSim.alphaTarget(0.0001);
     //d.fx = null;
     //d.fy = null;
-  }
+}
 
-  dragLine(d, n) {
+dragLine(d, n) {
     this._dragline.attr('d', 'M' + d.x + ',' + d.y + 'L' + d3.event.x + ',' + d3.event.y);
-  }
+}
 
-  dragLineVisibility(v) {
+dragLineVisibility(v) {
     this._dragline.classed('hidden', !v);
-  }
+}
 
-  onMouseUp() {
+onMouseUp() {
 
     this._fsm.evaluate("mouseup");
 
-  }
+}
 
-  onKeyDown() {
+onKeyDown() {
 
     switch (d3.event.keyCode) {
       case 0x8:
-        this._fsm.evaluate("BACKSPACE_KEY");
-        break;
-    }
+      this._fsm.evaluate("BACKSPACE_KEY");
+      break;
   }
+}
 
-  onKeyUp() {}
+onKeyUp() {}
 
-  addNode() {
+addNode() {
 
     var point = d3.mouse(this._svg.node());
     var count = this._graph.nodes.length;
@@ -266,75 +274,75 @@ export class GraphEditor {
     //this._graph.links.pop();
     this.renderGraph(this._graph);
 
-  }
+}
 
-  addRelation(d, n) {
+addRelation(d, n) {
     var newRel = { source: this._nodeSelection, target: this._targetNode };
     //this._graph.links.push(newRel);
     this._graph.addLink(this._nodeSelection, this._targetNode);
     this.dragLineVisibility(false);
     this.renderGraph(this._graph);
-  }
+}
 
-  deleteSelection(d, n) {
+deleteSelection(d, n) {
     //
     // delete nodes
     //
     if (this._nodeSelection != null) {
       this._graph.nodes.splice(this._graph.nodes.indexOf(this._nodeSelection), 1);
       this.spliceLinksForNode(this._nodeSelection);
-    }
+  }
     //
     // delete relations
     //
     if (this._relationSelection != null) {
       this._graph.links.splice(this._graph.links.indexOf(this._relationSelection), 1);
-    }
-    this.renderGraph(this._graph);
   }
+  this.renderGraph(this._graph);
+}
 
-  spliceLinksForNode(node) {
+spliceLinksForNode(node) {
     var toSplice = this._graph.links.filter((l) => {
       return (l.source === node || l.target === node);
-    });
+  });
     toSplice.map((l) => {
       this._graph.links.splice(this._graph.links.indexOf(l), 1);
-    });
-  };
+  });
+};
 
-  highlightNode(d, n) {
+highlightNode(d, n) {
     //d.classed('update');
     this._targetNode = d;
     d3.select(n).classed('over', true);
-  }
+}
 
-  unHighlightNode(d, n) {
+unHighlightNode(d, n) {
     //d.classed('update');
     d3.select(n).classed('over', false);
-  }
+}
 
-  selectNode(d, n) {
+selectNode(d, n) {
     console.log('Node selected: ' + d);
 
     this.unselectAll();
     this.unHighlightNode(d, n);
     d3.select(n).classed('selected', true);
     this._nodeSelection = d;
-  }
+}
 
-  selectLink(d, n) {
+selectLink(d, n) {
     this.unselectAll();
     d3.select(n).classed('selected', true);
     this._relationSelection = d;
-  }
+}
 
-  unselectAll() {
+unselectAll() {
     d3.selectAll(".node, .link").classed('selected', false);
     this._nodeSelection = null;
     this._relationSelection = null;
-  }
+}
 
-  addTickListener(listener) {
+addTickListener(listener) {
     this._tickListeners.push(listener);
-  }
+}
 } /* /class */
